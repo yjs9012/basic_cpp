bb9f5d8392e6ccee03be8bb5eafb2ffb7cef348d
Jinsub Yim <yjs9012@gmail.com>
2023-02-02 00:19:01 -0500

    FIRST COMMIT

---
+++ 0_basic/0_1_reference.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+/******************************************************************************
+ 1. int& : int형 변수 참조자 | double& : double형 변수 참조자
+ 2. 레퍼런스는  define과 동시에 declare 해주어야 함. ex) int& ref_a = a
+ 3. 한번 다른 변수의 레퍼런스가 되면 다시 바꿀수 없음.
+ 4. 포인터는 메모리상 8byte이지만, 레퍼런스는 메모리를 차지할수도 있고 안할수도 있다.
+ 5. (장점) 포인터와 다르게 불필요한 &와 *가 필요없다.
+ 6. 상수값을 참조하기 위해서는 const int&ref = 4; 와 같이 상수 참조자 선언
+******************************************************************************/
+int change_val(int *p) // pointer
+{
+    *p = 7;
+
+    return 0;
+}
+
+int main()
+{
+    int a = 3;
+    int &another_a = a; // reference
+
+    another_a = 5;
+    std::cout << "a : " << a << std::endl;
+    std::cout << "another_a : " << another_a << std::endl;
+    change_val(&a);
+    std::cout << "using_pointer_a : " << a << std::endl;
+
+    return 0;
+}
---
+++ 0_basic/0_2_various_reference.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+/******************************************************************************
+ 1. int&y = x; int&z = y --> int&&z = x(NO) : 참조자의 참조자 만들 필요가 없다.
+ 2. (Warning) 레퍼런스의 레퍼런스, 레퍼런스의 배열, 레퍼런스의 포인터는 존재할수 없다!
+ 3. (Warning) 레퍼런스를 리턴하는 함수에서, 함수내에 정의된 지역변수를 리턴하지 않도록 주의!
+              값을 리턴하는 함수에서, const 레퍼런스로 리턴값을 받으면 지역변수 life 연장
+ 4. 참고 사이트 : https://modoocode.com/141
+******************************************************************************/
+int &function_a(int &a)
+{
+    a = 5;
+    return a;
+}
+
+int function_b()
+{
+    int a = 5;
+    return a;
+}
+
+int main()
+{
+    int x;
+    int &y = x;
+    int &z = y;
+
+    x = 1;
+    std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+    y = 2;
+    std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+    z = 3;
+    std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+    /***********************************************************************/
+    /**********************<References of Arrays>***************************/
+    int arr[2] = {1, 2};
+    int(&ref)[2] = arr;
+
+    std::cout << "arr[0] : " << arr[0] << " ref[0] : " << ref[0] << std::endl;
+    std::cout << "arr[1] : " << arr[1] << " ref[1] : " << ref[1] << std::endl;
+    /***********************************************************************/
+    /********************<Function return Reference>************************/
+    int b = 2;
+    std::cout << "b : " << b << std::endl;
+    int c = function_a(b);
+    std::cout << "b : " << b << " c : " << function_a(b) << std::endl;
+
+    const int &ref_a = function_b();
+    std::cout << "ref_a : " << ref_a << std::endl;
+
+    return 0;
+}
